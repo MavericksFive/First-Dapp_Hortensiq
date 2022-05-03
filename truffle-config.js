2f97070490303daa450e20e243362bd0833b4b80
Arnaud <95299145+MavericksFive@users.noreply.github.com>
2022-05-03 23:07:07 +0200

    Update Hortensia Dapp header

--- truffle-config.js
+++ truffle-config.js
@@ -1,3 +1,6 @@
+var HDWalletProvider = require("/Users/arnaudberger/hortensiq/Client/node_modules/truffle-hdwallet-provider");
+const mnemonic = "a09d4f033c237e2b5afdbee167fa3942e52dc6a61deb9cf29e77cf7f8b5cb667";
+
 module.exports = {
   networks: {
     development: {
@@ -5,7 +8,7 @@ module.exports = {
       port: 7545,
       network_id: "*", // Match any network id
       gas: 5000000
-    }
+    },
   },
   contracts_directory: "/Users/arnaudberger/hortensiq/Client/src/contracts",
   contracts_build_directory: "/Users/arnaudberger/hortensiq/Client/src/Abis",
